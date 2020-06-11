561b6051a7adb378f90b76139bcb656da2b887c9
dev2 <habib@gmail.com>
2020-06-11 16:24:25 +0530

    adding setter getter

--- EmployeeInfo/src/main/java/com/app/emp/Employee.java
+++ EmployeeInfo/src/main/java/com/app/emp/Employee.java
@@ -6,5 +6,42 @@ public class Employee {
 	private String name;
 	private String add;
 	
+	private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAdd() {
+		return add;
+	}
+
+	public void setAdd(String add) {
+		this.add = add;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
 	
 }
+
